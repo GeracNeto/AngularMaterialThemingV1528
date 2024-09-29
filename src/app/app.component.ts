fa16b406b104dc7d9b21f67959554056104d3500
Geraldo Pereira da Costa Neto <gera.cost18@gmail.com>
2024-09-29 18:59:48 -0300

    adds angular material, creates my-theme file that creates darh and light theme and implements theme toggle;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'AngularMaterialThemingV1528';
+  isDarkTheme = true;
+
+  ngOnInit() {
+    document.body.classList.add('dark-theme');
+  }
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    document.body.classList.toggle('dark-theme', this.isDarkTheme);
+    document.body.classList.toggle('light-theme', !this.isDarkTheme);
+  }
 }
---
+++ src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [MatSidenavModule, MatButtonModule, MatExpansionModule],
+})
+export class MaterialModule {}
